fff878ef0401771d81e99957f5d4634ed2b1a24b
Felix Ortner <110030617+Heroooz@users.noreply.github.com>
2024-09-19 15:38:08 +0200

    Update mult.c

--- mult.c
+++ mult.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
-  return 0;
+    double result;
+    if (is_number(argv[1] && is_number(argv[2]))){
+        result = (int)argv[1] * (int)argv[2];
+    }
+    else{
+        result = -1;
+    }
+  return result;
 }
+
+int is_number(const char *str) { 
+    char *endptr; 
+    double value = strtod(str, &endptr); 
+ 
+    // Check if the entire string was a valid number 
+    if (endptr == str) { 
+        return 0; // Not a number 
+    } 
+ 
+    return 1; // Is a number 
+} 

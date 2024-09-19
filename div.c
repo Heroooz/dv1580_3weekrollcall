80ccf9874aa09956671d1cfc33a977b775d70d4f
Felix Ortner <110030617+Heroooz@users.noreply.github.com>
2024-09-19 15:43:05 +0200

    Update div.c

--- div.c
+++ div.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
-  return 0;
+    double result;
+    if (is_number(argv[1] && is_number(argv[2]))){
+        result = (double)argv[1] / (double)argv[2];
+    }
+    else{
+        printf("DIV ZERO NOT ALLOWED!\n");
+        result = 0;
+    }
+  return result;
 }
+
+int is_number(const char *str) { 
+    char *endptr; 
+    double value = strtod(str, &endptr); 
+ 
+    // Check if the entire string was a valid number 
+    if (endptr == str) { 
+        return 0; // Not a number 
+    } 
+ 
+    return 1; // Is a number 
+} 
